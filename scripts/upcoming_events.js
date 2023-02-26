bae03ca2c20df7370507266d830c01b1afda987d
Nani <nany.nagisa@gmail.com>
2023-02-25 22:24:20 -0300

    card future events

---
+++ scripts/upcoming_events.js
@@ -0,0 +1,41 @@
+import data from './data.json' assert {type: 'json'};
+
+let cards = document.getElementById("cards");
+let today = new Date(data.currentDate);
+
+
+for (let card of data.events.filter(event => {
+    let eventDate = new Date(event.date)
+    return eventDate > today
+})) {
+  cards.innerHTML += `
+  <div class="card" style="width: 20rem">
+  <img
+    src="${card.image}"
+    class="card-img-top"
+    alt="${card.name}"
+  />
+  <div class="card-body">
+    <div class="title">
+      <h5 class="card-title">${card.name}</h5>
+      <h6>${card.date}</h6>
+    </div>
+    <p class="card-text">
+    ${card.description}
+    </p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Category: ${card.category}</li>
+    <li class="list-group-item">Place: ${card.place}</li>
+    <li class="list-group-item">Capacity: ${card.capacity}</li>
+    <li class="list-group-item">${card.assistance ? "Assistance" : "Estimate"}: ${card.assistance ? card.assistance : card.estimate} </li>
+    <li class="list-group-item">
+      <div class="price-container">
+        <p>Price: $${card.price}</p>
+        <button type="button" class="btn btn-success">See More</button>
+      </div>
+    </li>
+  </ul>
+</div>
+  `;
+}
