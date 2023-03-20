91b878487af3fe0b0609a6d1138f09f0b631b383
Nani <nany.nagisa@gmail.com>
2023-03-19 22:54:27 -0300

    fetch ok in all

--- scripts/past_events.js
+++ scripts/past_events.js
@@ -1,45 +1,59 @@
-import data from './data.json' assert {type: 'json'};
+let cards = document.getElementById('cards');
+let cardEvents = [];
 
-let cards = document.getElementById("cards");
-let today = new Date(data.currentDate);
-
-
-for (let card of data.events.filter(event => {
-    let eventDate = new Date(event.date)
-    return eventDate < today
-})) {
-  cards.innerHTML += `
-  <div class="card" style="width: 20rem">
-  <img
-    src="${card.image}"
-    class="card-img-top"
-    alt="${card.name}"
-  />
-  <div class="card-body">
-    <div class="title">
-      <h5 class="card-title">${card.name}</h5>
-      <h6>${card.date}</h6>
-    </div>
-    <p class="card-text">
-    ${card.description}
-    </p>
-  </div>
-  <ul class="list-group list-group-flush">
-  <li class="list-group-item category" value="${card.category}">Category: ${card.category}</li>
-    <li class="list-group-item">Place: ${card.place}</li>
-    <li class="list-group-item">Capacity: ${card.capacity}</li>
-    <li class="list-group-item">${card.assistance ? "Assistance" : "Estimate"}: ${card.assistance ? card.assistance : card.estimate} </li>
-    <li class="list-group-item">
-      <div class="price-container">
-        <p>Price: $${card.price}</p>
-        <a href="../details.html?id=${card._id}"> <button type="button" class="btn btn-success">See More</button> </a>
-      </div>
-    </li>
-  </ul>
-</div>
-  `;
+function dataBase() {
+  fetch('https://mindhub-xj03.onrender.com/api/amazing')
+    .then((response) => response.json())
+    .then((apiData) => {
+      cardCreator(apiData.events, cards);
+    })
+    .catch((error) => console.log(error.message));
 }
 
+dataBase();
+
+function cardCreator(cardData, place) {
+  let cardCreated = '';
+
+  cardData.forEach((card) => {
+    cardCreated += `
+    <div class="card" style="width: 20rem; margin-top: 1rem">
+     <img src="${card.image}" class="card-img-top" alt="${card.name}"/>
+    <div class="card-body">
+      <div class="title">
+        <h5 class="card-title">${card.name}</h5>
+        <h6>${card.date}</h6>
+      </div>
+      <p class="card-text">
+      ${card.description}
+      </p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item category" value="${card.category}">Category: ${
+      card.category
+    }</li>
+      <li class="list-group-item">Place: ${card.place}</li>
+      <li class="list-group-item">Capacity: ${card.capacity}</li>
+      <li class="list-group-item">${
+        card.assistance ? 'Assistance' : 'Estimate'
+      }: ${card.assistance ? card.assistance : card.estimate} </li>
+      <li class="list-group-item">
+        <div class="price-container">
+          <p>Price: ${card.price}</p>
+          <a href="../details.html?id=${
+            card._id
+          }"> <button type="button" class="btn btn-success">See More</button> </a>
+          
+          </div>
+      </li>
+    </ul>
+  </div>
+    `;
+  });
+  place.innerHTML = cardCreated;
+}
+
+dataBase();
 
 //Searcher
 let searcherInput = document.getElementById('searcher');
@@ -64,36 +78,36 @@ buttonSearch.addEventListener('click', () => {
   searchEvent(searcherInput.value);
 });
 
+
 //checkboxes
-let checkboxes = document.querySelectorAll(".checkbox-values")
-let categories = document.querySelectorAll(".category")
-let array = []
+let checkboxes = document.querySelectorAll('.checkbox-values');
+let array = [];
 
-checkboxes.forEach(e=>{
-  e.addEventListener("click", ()=> {
-    if(e.checked){
-      array.push(e.value)
-    } 
-    if(e.checked === false) {
-      let newarray = array.filter(el => el !== e.value)
-      array = newarray
-    }
-    categoryFilter(array)
-  })
-})
-
-function categoryFilter(array) {
-  if(array.length){
-    for (let i = 0; i < categories.length; i++) {  
-      if (array.includes(categories[i].getAttribute("value"))) {
+function categoryFilter(array, categories) {
+  if (array.length) {
+    for (let i = 0; i < categories.length; i++) {
+      if (array.includes(categories[i].getAttribute('value'))) {
         categories[i].parentNode.parentNode.style.display = 'flex';
       } else {
         categories[i].parentNode.parentNode.style.display = 'none';
       }
     }
-  }else {
+  } else {
     for (let i = 0; i < categories.length; i++) {
       categories[i].parentNode.parentNode.style.display = 'flex';
     }
   }
 }
+checkboxes.forEach((e) => {
+  e.addEventListener('click', () => {
+    let categories = document.querySelectorAll('.category');
+    if (e.checked) {
+      array.push(e.value);
+    }
+    if (e.checked === false) {
+      let newarray = array.filter((el) => el !== e.value);
+      array = newarray;
+    }
+    categoryFilter(array, categories);
+  });
+});
