c94b641b7286093763d721eae968728072c52939
Nani <nany.nagisa@gmail.com>
2023-03-21 16:04:00 -0300

    Almost Table

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,147 +1,93 @@
-let arrayPast=[]
-fetch("https://mindhub-xj03.onrender.com/api/amazing")
-.then(res => res.json())
-.then (data => {
-  arrayPast = past(data.events, data.currentDate)
-  console.log(arrayPast)
-//let percentage = assistance(arrayPast)
+fetch('https://mindhub-xj03.onrender.com/api/amazing')
+  .then((response) => response.json())
+  .then((data) => {
+    printTable1(
+      results(assistance(data), assistance(data).reverse(), capacity(data)),
+      'table1'
+    );
+    printTable2(getCategory(upcoming(data)), 'table2');
+  });
 
-
-
-})
-
-function past(data,currentDate){
-return data.filter(event => event.date < currentDate)
+function upcoming(data) {
+  let event = data.events;
+  let currentDate = data.currentDate;
+  return event.filter((event) => event.date > currentDate);
 }
 
-function assistance(arrPast){
-  const arrayPercentage = arrPast.map(event=>{
-    return{
-      attendance: (event.assistance/event.capacity)*100,
-      nameEvent:event.name
+function getCategory(array) {
+  let categories = {};
+  for (let i = 0; i < array.length; i++) {
+    if (!categories.hasOwnProperty(array[i].category)) {
+      categories = {
+        ...categories,
+        [array[i].category]: {
+          ['revenues']: array[i].price * array[i].estimate,
+        },
+      };
+    } else {
+      let label = array[i].category;
+      console.log(categories.label);
+      //categories[array[i].category] = categories[array[i].category].revenues + (array[i].price * array[i].estimate)
     }
-  })
-arrayPercentage.sort((a,b)=>b.attendance - a.attendance)
-console.log(arrayPercentage)
-return arrayPercentage
-
+  }
+  return categories;
 }
 
-function capacity(arrPast) {
-  const arrayCapacity =arrPast.map(event => {
+function past(data) {
+  return data.filter((event) => event.date < data.currentDate);
+}
+
+function assistance(arrayToRead) {
+  let arrayPercentage = arrayToRead.events.map((event) => {
+    return {
+      attendance: (event.assistance / event.capacity) * 100,
+      nameEvent: event.name,
+    };
+  });
+  arrayPercentage.sort((a, b) => b.attendance - a.attendance);
+  return arrayPercentage;
+}
+
+function capacity(arrayToRead) {
+  const arrayCapacity = arrayToRead.events.map((event) => {
     return {
       capacity: event.capacity,
-nameEvent:event.name
-    }})
-    arrayCapacity.sort((a,b)=>b.capacity - a.capacity)
-    console.log(arrayCapacity)
-    return arrayPercentage
-  
+      nameEvent: event.name,
+    };
+  });
+  arrayCapacity.sort((a, b) => b.capacity - a.capacity);
+  return arrayCapacity;
 }
 
+function results(highestPercentage, lowestPercentage, largerCapacity) {
+  let all = {
+    highestPercentage: highestPercentage[0].nameEvent,
+    lowestPercentage: lowestPercentage[0].nameEvent,
+    largerCapacity: largerCapacity[0].nameEvent,
+  };
 
-
-
-
-
-
-
-
-
-
-/* if(greaterCapacity < (event.capacity)){
-  greaterCapacity = event.capacity;
-  greaterCapacityList.push({capacityMax: greaterCapacity, event: event.name}) */
-
-/* let statsTable = document.getElementById("main-stats")
-
-function dataBase() {
-  fetch('https://mindhub-xj03.onrender.com/api/amazing')
-    .then((response) => response.json())
-    .then((apiData) => {
-      cardCreator(apiData.events, statsTable);
-    })
-    .catch((error) => console.log(error.message));
+  return all;
 }
-dataBase(); */
 
+function printTable1(results, element) {
+  let table1 = document.getElementById(element);
+  table1.innerHTML = `
+    <tr>
+        <td>${results.highestPercentage}</td>
+        <td>${results.lowestPercentage}</td>
+        <td>${results.largerCapacity}</td>
+    </tr>
+    `;
+}
 
-
-  /* <div class="table-responsive">
-      <table class="table table-container">
-        <tr>
-          <th colspan="3">Events statistics</th>
-        </tr>
-        <tr class="table-up">
-          <td>Events with the highest percentage of attendance</td>
-          <td>Events with the lovest percentage of attendance</td>
-          <td>Event with the larger Capacity</td>
-        </tr>
-        <tr class="table-down">
-          <td>Event1</td>
-          <td>Event2</td>
-          <td>Event3</td>
-        </tr>
-        <tr>
-          <th colspan="3">Upcoming events statistics by category</th>
-        </tr>
-        <tr class="table-up">
-          <td>Categories</td>
-          <td>Revenues</td>
-          <td>Percentage of attendance</td>
-        </tr>
-
-        <tr class="table-down">
-          <td>category1</td>
-          <td>revenue1</td>
-          <td>percentage1</td>
-        </tr>
-        <tr class="table-down">
-          <td>category2</td>
-          <td>revenue2</td>
-          <td>percentage2</td>
-        </tr>
-        <tr class="table-down">
-          <td>category3</td>
-          <td>revenue3</td>
-          <td>percentage3</td>
-        </tr>
-
-        <tr>
-          <th colspan="3">Past Events statistics by category</th>
-        </tr>
-
-        <tr class="table-up">
-          <td>Categories</td>
-          <td>Revenues</td>
-          <td>Percentage of attendance</td>
-        </tr>
-
-        <tr class="table-down">
-          <td>category1</td>
-          <td>revenue1</td>
-          <td>percentage1</td>
-        </tr>
-        <tr class="table-down">
-          <td>category2</td>
-          <td>revenue2</td>
-          <td>percentage2</td>
-        </tr>
-        <tr class="table-down">
-          <td>category3</td>
-          <td>revenue3</td>
-          <td>percentage3</td>
-        </tr>
-        <tr class="table-down">
-          <td>category4</td>
-          <td>revenue4</td>
-          <td>percentage4</td>
-        </tr>
-        <tr class="table-down">
-          <td>category5</td>
-          <td>revenue5</td>
-          <td>percentage5</td>
-        </tr>
-
-      </table>
-    </div> */
+function printTable2(object, element) {
+  let table1 = document.getElementById(element);
+  let array = Object.entries(object);
+  for (let i = 0; i < array.length; i++) {
+    table1.innerHTML += `
+        <td>${array[i][0]}</td>
+        <td>${array[i][1]}</td>
+        <td></td>
+    `;
+  }
+}
